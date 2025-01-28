926e9626c960b0b220a9f8d10e4c67abc6dc0643
agustin <schlotthaueraa@gmail.com>
2025-01-28 11:53:36 -0300

    agregue ignore

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-let numeros={}
-for (let i=1; i<=10; i++){
-    const numeroRandom = Math.round(Math.random() * 10 + 1)
-    numeros[numeroRandom]= numeros[numeroRandom]+1 || 1
-}
+//let numeros={}
+//for (let i=1; i<=10; i++){
+    //const numeroRandom = Math.round(Math.random() * 10 + 1)
+  //  numeros[numeroRandom]= numeros[numeroRandom]+1 || 1
+//}
 
-console.log(numeros)
-console.log("Fin del proceso.")
+//console.log(numeros)
+//console.log("Fin del proceso.")
 
 
 
-const crypto = require("crypto")
-const saludo= ()=> {
-    console.log("hola como estas")
+//const crypto = require("crypto")
+//const saludo= ()=> {
+//    console.log("hola como estas")
 
-}
+//}
 
-module.export ={
-    
-}
+//module.export ={
+
+//}
+
+
+
+
+
+import moment from 'moment'
+const today= moment()
+const cumple= moment([2000,5,6])
+console.log (cumple.isValid())
+
+const diff=today.diff(cumple, 'years')
+console.log({diff})
