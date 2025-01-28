23a20cf3cbae98834d1c55885c5144797fc6072b
agustin <schlotthaueraa@gmail.com>
2025-01-28 11:31:17 -0300

    instale librerias

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hola")
+let numeros={}
+for (let i=1; i<=10; i++){
+    const numeroRandom = Math.round(Math.random() * 10 + 1)
+    numeros[numeroRandom]= numeros[numeroRandom]+1 || 1
+}
+
+console.log(numeros)
+console.log("Fin del proceso.")
+
+
+
+const crypto = require("crypto")
+const saludo= ()=> {
+    console.log("hola como estas")
+
+}
+
+module.export ={
+    
+}
---
+++ nose.js
@@ -0,0 +1,8 @@
+//const {saludo} = require('./index.js')
+//export const saludo =()=>{
+
+//}
+//export default
+//export class 
+//import saludo from './index.js'
+
